d2a9625ec185ae993f5305d249e99eedf49361db
Ghiles Meddour <ghiles.meddour.b@gmail.com>
2021-10-11 19:11:30 +0200

    release 0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+## Version 0.1.0 (2021-10-11)
+- 🎉 first release!
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="cfof",
-    version="0.0.0",
+    version="0.1.0",
     author="Ghiles Meddour",
     author_email="ghiles.meddour@munic.io",
     description="Concentration Free Outlier Factor",
