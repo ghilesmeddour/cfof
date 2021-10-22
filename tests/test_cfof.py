5e5215deff5f16800539e7fcf6877402a33ec932
Ghiles Meddour <ghiles.meddour.b@gmail.com>
2021-10-22 09:11:37 +0200

    add simple unit tests

--- tests/test_cfof.py
+++ tests/test_cfof.py
@@ -4,5 +4,14 @@ from cfof import CFOF
 
 
 class TestCFOF:
-    def test_cfof(self):
-        pass
+    def test_cfof_simple(self):
+        expected_scores = np.array([[0.5,
+                                     0.66666667], [0.33333333, 0.83333333],
+                                    [0.5, 1.], [0.5, 0.66666667],
+                                    [0.33333333, 0.83333333], [0.5, 1.]])
+
+        X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+        cfof_clf = CFOF(metric='euclidean', rhos=[0.5, 0.6], n_jobs=1)
+        scores = cfof_clf.compute(X)
+
+        assert np.allclose(expected_scores, scores)
--- tests/test_fast_cfof.py
+++ tests/test_fast_cfof.py
@@ -4,5 +4,19 @@ from cfof import FastCFOF
 
 
 class TestFastCFOF:
-    def test_fast_cfof(self):
-        pass
+    def test_fast_cfof_simple(self):
+        expected_score_0_0 = 0.00954095
+
+        np.random.seed(10)
+        X = np.random.randint(0, 100, size=(1000, 3))
+
+        fast_cfof_clf = FastCFOF(metric='euclidean',
+                                 rhos=[0.001, 0.005, 0.01, 0.05, 0.1],
+                                 epsilon=0.1,
+                                 delta=0.1,
+                                 n_bins=50,
+                                 n_jobs=1)
+
+        scores = fast_cfof_clf.compute(X)
+
+        assert np.isclose(scores[0][0], expected_score_0_0)
